ef837f322ff3a873cbd04e3b7160d3de0fdedfc7
EQuarcoo <ephraimquarcoo70@gmail.com>
2024-04-25 11:57:44 +0000

    Create Page routing

---
+++ angular-routing/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from './navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, NavbarComponent, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-routing';
+}
